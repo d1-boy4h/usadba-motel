6adb6c0826644e656c95a9ae6590abc1e6e02a4e
Di <116650495+d1-boy4h@users.noreply.github.com>
2023-09-22 18:40:15 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 "use strict";
 
+/* ---------------- Стрелочка наверх ---------------- */
+
+window.addEventListener('scroll', function() {
+  var headerHeight = document.querySelector('.header').offsetHeight;
+
+  if (window.scrollY > headerHeight / 2) {
+    document.querySelector('.arrow_up').classList.add('visible');
+  }
+  else {
+    document.querySelector('.arrow_up').classList.remove('visible');
+  }
+});
+
 /* ---------------- Проверка видимости (для анимаций) ---------------- */
 
 function onEntry(entry) {
@@ -18,3 +31,9 @@ let elements = document.querySelectorAll('.animation');
 for (let elm of elements) {
   observer.observe(elm);
 }
+
+/* ---------------- Кнопка Забронировать ---------------- */
+
+document.getElementById("open-modal").addEventListener("click", function() {
+    document.getElementById("modal").classList.add("show");
+});
